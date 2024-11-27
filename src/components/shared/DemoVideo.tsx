54c4c72b400a16823a789a0afec0fa3840eee916
sarmueil <sarmuelypmd@gmail.com>
2024-11-27 13:39:25 +0100

    demo-video-upload

--- src/components/shared/DemoVideo.tsx
+++ src/components/shared/DemoVideo.tsx
@@ -3,11 +3,13 @@ const DemoVideo = () => {
     <div className="w-full h-full my-6">
       <div className="w-full md:h-[600px] px-4 md:px-0">
         <div className="max-w-6xl h-full bg-accent mx-auto rounded-3xl">
-          <video
-            src="/assets/demo.mp4"
-            controls
+          <iframe
+            src="https://www.youtube.com/embed/rGDPsrP18Go"
+            title="YouTube Video"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
             className="w-full aspect-video h-full rounded-3xl"
-          ></video>
+          ></iframe>
         </div>
       </div>
     </div>
