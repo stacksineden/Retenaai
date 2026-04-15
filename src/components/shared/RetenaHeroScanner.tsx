80e091eadbff9905bf86a60fac465f56b52e99b2
stacksineden <stacksineden@gmail.com>
2026-04-15 19:29:48 +0100

    updates

--- src/components/shared/RetenaHeroScanner.tsx
+++ src/components/shared/RetenaHeroScanner.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import  { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
 import { useUserCountry } from "@/hooks/useUserLocation";
